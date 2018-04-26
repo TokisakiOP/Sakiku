68b25782ab78b5e60f6992006096ea679d3ac4fb
Cristian <cristianrodriguez@imasdetres.com>
2018-04-26 14:25:11 +0200

    arreglo

---
+++ Sakiku/app/src/main/java/com/example/tokisaki/sakiku/Creditos.java
@@ -0,0 +1,142 @@
+package com.example.tokisaki.sakiku;
+
+/**
+ * Created by Tokisaki on 15/04/2018.
+ */
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.view.MotionEvent;
+
+/***
+ * Escena que contiene los créditos del juego
+ */
+public class Creditos extends Escenas {
+
+    /**
+     * pincel para el dibujo de las fuentes externa
+     */
+    private Paint p;
+
+    /**
+     * pincel para el dibujo de las fuentes externa
+     */
+    private Paint l;
+
+    /**
+     * Layout contenedor de texto
+     */
+    private StaticLayout layoutDescripcion;
+
+    /**
+     * Pincel para escritura
+     */
+    private TextPaint pDescripcion;
+
+    /**
+     * Variable que recogerá "creditos" de strings.xml
+     */
+    private String creditos;
+
+    /**
+     * Variable que recogerá "creditosTexto" de strings.xml
+     */
+    private String creditosTexto;
+
+    /**
+     * Ancho del contenedor de descripcion
+     */
+    private int anchoLayoutDescripcion;
+
+    /**
+     * Posicion de dibujo de la cabecera Creditos
+     */
+    private Point posCreditos ;
+
+    /**
+     * Posicion de dibujado de la descripcion
+     */
+    private Point posDescripcion ;
+
+    /***
+     * Constructor de la clase
+     * @param numEscena numero de la escena
+     * @param context contexto de la aplicación
+     * @param anchoPantalla ancho de la pantalla del dispositivo donde se ejecita la aplicación
+     * @param altoPantalla alto de la pantalla del dispositivo donde se ejecita la aplicación
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public Creditos(int numEscena, Context context, int anchoPantalla, int altoPantalla) {
+        super(numEscena, context, anchoPantalla, altoPantalla);
+        inicializar();
+    }
+
+    /***
+     * función que inicializa los elementos de la pantalla
+     */
+    private void inicializar() {
+        creditos = context.getResources().getString(R.string.creditosTitulo);
+        //agradecimientos = context.getResources().getString(R.string.agredecimientos);
+        creditosTexto = context.getResources().getString(R.string.creditosTexto);
+        //agradecimientosTexto = context.getResources().getString(R.string.agredecimientosTexto);
+        this.pDescripcion = new TextPaint();
+        this.pDescripcion.setColor(Color.RED);
+        this.pDescripcion.setTextSize(getPixels(20));
+        this.pDescripcion.setTextAlign(Paint.Align.LEFT);
+        pDescripcion.setTypeface(faw);
+
+        l = new Paint();
+        l.setColor(Color.RED);
+        l.setTextSize(getPixels(45));
+        l.setTypeface(letras);
+        p = new Paint();
+        p.setColor(Color.RED);
+        p.setTextSize(getPixels(40));
+        p.setTypeface(faw);
+
+        anchoLayoutDescripcion = anchoPantalla / 2;
+        posCreditos = new Point(anchoPantalla / 2 - getPixels(75), getPixels(80));
+        posDescripcion = new Point(anchoPantalla / 4, getPixels(130));
+    }
+
+    /***
+     * Dibujamos los elementos en pantalla
+     * @param c Lienzo sobre el que dibujar
+     */
+    public void dibujar(Canvas c) {
+        c.drawColor(Color.BLACK);
+        c.drawBitmap(fondo, 0, 0, null);
+        c.drawText(creditos, posCreditos.x, posCreditos.y, l);
+        this.layoutDescripcion = new StaticLayout(creditosTexto, pDescripcion, anchoLayoutDescripcion, Layout.Alignment.ALIGN_CENTER, 1.0f, 0.0f, false);
+        c.translate(posDescripcion.x, posDescripcion.y);
+        this.layoutDescripcion.draw(c);
+    }
+
+    /***
+     * Actualizamos la física de los elementos en pantalla
+     */
+    public void actualizarFisica() {
+        super.actualizarFisica();
+    }
+
+    /***
+     * Registra las pulsaciones en pantalla
+     * @param event Tipo de pulsación
+     * @return numero de la escena actual
+     */
+    public int onTouchEvent(MotionEvent event) {
+        super.onTouchEvent(event);
+        if (event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_POINTER_UP)
+            return 1;
+        return numEscena;
+    }
+}
+
---
+++ Sakiku/app/src/main/java/com/example/tokisaki/sakiku/Jugar.java
@@ -0,0 +1,278 @@
+package com.example.tokisaki.sakiku;
+
+/**
+ * Created by Tokisaki on 15/04/2018.
+ */
+
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.PointF;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.view.MotionEvent;
+
+import java.util.ArrayList;
+
+/***
+ * Clase que ejecuta los elementos relacionados con el juego , así como el juego en sí
+ */
+public class Jugar extends Escenas {
+
+    /**
+     * instancia de la clase Personaje
+     */
+    private Personaje corredor;
+
+    /**
+     * indica el intervalo de tiempo en el que se actualizan los frames
+     */
+    private long tiempoMove;
+
+    /**
+     * lista con los obstaculos que hay en pantalla
+     */
+    private ArrayList<Obstaculos> obstaculos;
+
+    /**
+     * tiempo de refresco de pantalla para los frames del disparo del personaje
+     */
+    private int tickDisparoFrame = 40;
+
+    /**
+     * puntuación conseguida
+     */
+    private int puntuacion = 0;
+
+    /**
+     * tiempo de refresco de pantalla para el movimento sobre el eje X del personaje
+     */
+    private int tickMove = 30;
+
+    /**
+     * volumen de la música
+     */
+    private int v;
+
+    /**
+     * numero que calcula cada cuantos frames suena el sonido de las pisadas
+     */
+    private int pasear = 0;
+
+    /**
+     * String que se muestra una vez que acaba la carrera
+     */
+    private String fin;
+
+    /**
+     * pincel para las fuentes externas
+     */
+    private Paint p;
+
+    /**
+     * pincel para las fuentes externas
+     */
+    private Paint l;
+
+    /**
+     * booleana que indica si se ha finalizado la carrera
+     */
+    private boolean finCarrera = false;
+
+    /**
+     * contador que los contiene 60 segundos que dura la carrera
+     */
+    private int contador = 60;
+
+    /**
+     * contador que le resta una unidad cada segundo a la variable "contador"
+     */
+    private long segundo;
+
+    /**
+     * Posicion del temporizador del juego
+     */
+    private Point posContador;
+
+    /**
+     * Posicion del texto fin
+     */
+    private Point posTextoFin;
+
+    /**
+     * Fondo de la pantalla
+     */
+    private FondoAnimado fondoAnimado;
+
+    /**
+     * BotonImagen para saltar
+     */
+    private BotonImagen btnSalto;
+
+    /**
+     * BotonImagen para deslizarse
+     */
+    private BotonImagen btnDesliz;
+
+    /**
+     * BotonImagen de disparo
+     */
+    private BotonImagen btnDisparo;
+
+    /**
+     * BotonImagen de disparo dos
+     */
+    private BotonImagen btnDisparoDos;
+
+    /***
+     * Constructor de la clase
+     * @param numEscena numero de la escena
+     * @param context contexto de la aplicación
+     * @param anchoPantalla ancho de la pantalla del dispositivo donde se ejecita la aplicación
+     * @param altoPantalla alto de la pantalla del dispositivo donde se ejecita la aplicación
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public Jugar(int numEscena, Context context, int anchoPantalla, int altoPantalla) {
+        super(numEscena, context, anchoPantalla, altoPantalla);
+        inicializar();
+        obstaculos = new ArrayList<>();
+        corredor = new Personaje(context, altoPantalla, anchoPantalla);
+        tiempoMove = System.currentTimeMillis();
+        segundo = System.currentTimeMillis() + 1000;
+        fondoAnimado = new FondoAnimado(context, anchoPantalla, altoPantalla);
+
+
+        //comenzo = true;
+        //v= audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+    }
+
+    /***
+     * Inicializa los elementos necesarios para la clase
+     */
+    private void inicializar() {
+
+        fin = context.getResources().getString(R.string.Final);
+        p = new Paint();
+        p.setColor(Color.RED);
+        p.setTextSize(getPixels(50));
+        p.setTypeface(faw);
+        l = new Paint();
+        l.setColor(Color.RED);
+        l.setTextSize(getPixels(30));
+        l.setTypeface(letras);
+
+        btnDisparo = new BotonImagen(escalaAnchura(BitmapFactory.decodeResource(context.getResources(), R.drawable.senal), getPixels(80)),
+                new Point(anchoPantalla - escalaAnchura(BitmapFactory.decodeResource(context.getResources(), R.drawable.senal), getPixels(80)).getWidth(), 0));
+        btnDisparoDos = new BotonImagen(escalaAnchura(BitmapFactory.decodeResource(context.getResources(), R.drawable.lunafinal), getPixels(80)),
+                new Point(anchoPantalla - btnDisparo.imagen.getHeight(), escalaAnchura(BitmapFactory.decodeResource(context.getResources(), R.drawable.lunafinal), getPixels(80)).getHeight()));
+        btnDesliz = new BotonImagen(escalaAnchura(BitmapFactory.decodeResource(context.getResources(), R.drawable.deslizarse), getPixels(80)),
+                new Point(0, altoPantalla - escalaAnchura(BitmapFactory.decodeResource(context.getResources(), R.drawable.deslizarse), getPixels(80)).getHeight()));
+        btnSalto = new BotonImagen(escalaAnchura(BitmapFactory.decodeResource(context.getResources(), R.drawable.saltar), getPixels(80)),
+                new Point(0, altoPantalla - btnDesliz.imagen.getHeight() * 2));
+        posContador = new Point(getPixels(20), getPixels(30));
+        posTextoFin = new Point(anchoPantalla / 2, altoPantalla / 2);
+    }
+
+
+    /***
+     * Actualizamos la física de los elementos en pantalla
+     */
+    public void actualizarFisica() {
+        if (contador <= 0) {
+            finCarrera = true;
+        }
+        if (!finCarrera) {
+            if (corredor.comprobarVictoria(anchoPantalla)) {
+                finCarrera = true;
+            } else {
+                fondoAnimado.actualizarFisica();
+                corredor.desplazarse(true);
+                if (System.currentTimeMillis() > segundo) {
+                    contador--;
+                    segundo = System.currentTimeMillis() + 1000;
+                }
+                if (System.currentTimeMillis() - tiempoMove > tickMove) {
+                    corredor.actualizarFisica();
+                    tiempoMove = System.currentTimeMillis();
+                }
+                for (Obstaculos obs : obstaculos) {
+                    if (obs.actualizarFisica(anchoPantalla)) {
+                        obstaculos.remove(obs);
+                        //Aqui por leer en orden fuerza a esperar a la siguiente ejecucion para seguir trabajando con el resto
+                        //si en vez de 50 fps , fueses a 1 , verias el resto de disparos quietos 1 segundo entero :S
+                        break;
+                    }
+                    if (obs.detectarColision(corredor)) {
+                        obstaculos.remove(obs);
+                        corredor.desplazarse(false);
+                        corredor.setRectangulos();
+                    }
+                }
+            }
+        }
+    }
+
+    /***
+     * Dibujamos los elementos en pantalla
+     * @param canvas Lienzo sobre el que dibujar
+     */
+    public void dibujar(Canvas canvas) {
+        if (!finCarrera) {
+            canvas.drawColor(Color.BLUE);
+            fondoAnimado.dibujar(canvas);
+            canvas.drawText("" + contador, posContador.x, posContador.y, l);
+            btnDisparo.dibujar(canvas);
+            btnDisparoDos.dibujar(canvas);
+            btnDesliz.dibujar(canvas);
+            btnSalto.dibujar(canvas);
+            for (Obstaculos obs : obstaculos) {
+                obs.dibujar(canvas);
+            }
+            corredor.dibujar(canvas);
+        } else {
+            canvas.drawColor(Color.BLACK);
+            canvas.drawText(fin, posTextoFin.x, posTextoFin.y, l);
+        }
+    }
+
+    /***
+     * Registra las pulsaciones en pantalla
+     * @param event Tipo de pulsación
+     * @return numero de la escena actual
+     */
+    @Override
+    public int onTouchEvent(MotionEvent event) {
+        int x = (int) event.getX(), y = (int) event.getY();
+        switch (event.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                if (!finCarrera) {
+                    if (btnDesliz.collider.contains(x, y)) {
+                        corredor.setEstado(eEstadoPersonaje.DESLIZANDOSE);
+                    } else if (btnSalto.collider.contains(x, y)) {
+                        corredor.setEstado(eEstadoPersonaje.SALTANDO);
+                    } else if (btnDisparo.collider.contains(x, y)) {
+                        obstaculos.add(new Obstaculos(context, new PointF(anchoPantalla, altoPantalla - corredor.frameActual.getHeight()), true));
+                    } else if (btnDisparoDos.collider.contains(x, y)) {
+                        //obstaculos.add(new Obstaculos(context, new PointF(anchoPantalla, altoPantalla - getPixels(40)), false));
+                        //TODO salta excepcion , a revisar
+                    }
+                } else {
+                    return 1;
+                }
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+        return numEscena;
+    }
+}
+
+
+
