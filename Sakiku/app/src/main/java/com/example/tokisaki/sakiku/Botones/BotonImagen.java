dfe7cb14ea5488cb733baeb7ac7a0da1f64915f8
Cristian <cristianrodriguez@imasdetres.com>
2018-04-26 14:04:35 +0200

    Documentada clase BotonImagen

--- Sakiku/app/src/main/java/com/example/tokisaki/sakiku/Botones/BotonImagen.java
+++ Sakiku/app/src/main/java/com/example/tokisaki/sakiku/Botones/BotonImagen.java
@@ -5,13 +5,32 @@ import android.graphics.Canvas;
 import android.graphics.Point;
 import android.graphics.Rect;
 
+/**
+ * Boton con imagen
+ */
 public class BotonImagen {
 
+    /**
+     * Imagen del boton
+     */
     public Bitmap imagen;
-    public Rect collider;
-    public Point posicion;
-    public String texto;
 
+    /**
+     * Collider del boton
+     */
+    public Rect collider;
+
+    /**
+     * Posicion de dibujo del boton
+     */
+    public Point posicion;
+
+    /**
+     * Inicializa una instancia de la clase segun parametros recibidos
+     *
+     * @param imagen   Imagen del boton
+     * @param posicion Posicion
+     */
     public BotonImagen(Bitmap imagen, Point posicion) {
 
         this.imagen = imagen;
@@ -19,6 +38,10 @@ public class BotonImagen {
         collider = new Rect(posicion.x, posicion.y, posicion.x + imagen.getWidth(), posicion.y + imagen.getHeight());
     }
 
+    /**
+     * Dibuja el boton sobre un lienzo recibido como parametro
+     * @param canvas Lienzo recibido
+     */
     public void dibujar(Canvas canvas) {
         canvas.drawBitmap(imagen, posicion.x, posicion.y, null);
     }
